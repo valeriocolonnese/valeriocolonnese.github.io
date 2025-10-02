4f7cbe54248a70e37945e634782514753859ab5c
Valerio Colonnese <valerio.colonnese@gmail.com>
2025-10-02 16:06:59 +0200

    PlayerName

--- Build/valeriocolonnese.github.io.loader.js
+++ Build/valeriocolonnese.github.io.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 83720337,
+    wasmFileSize: 83721324,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
